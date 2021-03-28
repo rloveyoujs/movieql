14abd20dbd49742d3d5e724954a075c9303c0a06
rloveyoujs <rloveyoujs@gmail.com>
2021-03-28 18:01:27 +0900

    Initial commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { resolvers } = require("./graphql/resolvers");
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  playground: true,
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
