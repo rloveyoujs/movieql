6db63a2e5fd73564a8598e9d572acf9c213ddebd
rloveyoujs <rloveyoujs@gmail.com>
2021-03-28 17:43:59 +0900

    Initial commit

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
